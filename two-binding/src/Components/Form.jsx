e3f1d4b842d07452e2e59e33a61f907669696221
minupatel95201-cmd <minupatel95201@gmail.com>
2026-03-16 17:18:46 +0530

    React

--- two-binding/src/Components/Form.jsx
+++ two-binding/src/Components/Form.jsx
@@ -6,13 +6,19 @@ const Form = () => {
   const [Password, setpassword] = useState('')
 
   function SubmitForm(){
-    
+    console.log("Data Send To Backend:", {"Email:":Email, "Password:": Password});
+
+    setemail("");
+    setpassword("");
   }
   return (
     <>
       <section className="flex justify-center items-center h-screen">
         <div className="bg-gray-200/50 shadow-2xl w-full max-w-sm p-2 h-auto rounded-t-4xl rounded-b-2xl">
-          <form className="bg-white text-black text-center rounded-3xl px-8 py-6">
+          <form className="bg-white text-black text-center rounded-3xl px-8 py-6"onSubmit={(e)=>{
+              e.preventDefault();
+              SubmitForm();
+          }}>
             <img src="https://i.pinimg.com/control1/1200x/6c/ac/7e/6cac7ebb906f588718fc66d1468c4e79.jpg" alt="logo" className="w-84 h-24 object-cover my-6" />
             <h1 className="text-3xl font-semibold my-2">Sign in to continue</h1>
             <p className="text-gray-400 text-sm mb-6">
@@ -24,6 +30,10 @@ const Form = () => {
                 className="outline-none  w-full px-3 py-2"
                 placeholder="Email"
                 type="email"
+                value={Email}
+                onChange={(e)=> {
+                     setemail(e.target.value);
+                }}
               />
             </div>
             <div className="text-gray-700 rounded-xl my-2 bg-gray-300 w-full flex items-center px-3">
@@ -32,6 +42,10 @@ const Form = () => {
                 className="outline-none  w-full px-3 py-2"
                 placeholder="Password"
                 type="password"
+                value={Password}
+                onChange={(e)=> {
+                    setpassword(e.target.value);
+                }}
               />
             </div>
             <input
