d3fbc8f52b34aa1f72c27a1c071a235d45b4ca82
minupatel95201-cmd <minupatel95201@gmail.com>
2026-03-16 16:56:11 +0530

    React

---
+++ two-binding/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Components/Form'
+
+const App = () => {
+  return (
+    <>
+    {/* Two Way Binding: Manage From Filed Using Usestate */}
+    <Form />
+    </>
+  )
+}
+
+export default App
